0b85e12e6595ba8a6d93a76e7ec6340825cd342b
Alonso del Arte <alonso.delarte@gmail.com>
2023-09-08 13:01:15 -0400

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Alonso del Arte on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
